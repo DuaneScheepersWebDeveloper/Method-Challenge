8bbd485a8deeb4584deece662c64efd439cdb8c0
Duane Scheepers <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2023-07-05 11:34:39 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MethodChallenge
+{
+    internal class Program
+    {
+
+      
+
+        public static void GreetFriend(string text) 
+        {
+            Console.WriteLine("Hi there, {0}",text);
+        }
+        static void Main(string[] args)
+        {
+            string friend1 = "Duane";
+            string friend2 = "Miguel";
+            string friend3 = "Harry";
+            GreetFriend(friend1);
+            GreetFriend(friend2);
+            GreetFriend(friend3);
+        }
+    }
+}
